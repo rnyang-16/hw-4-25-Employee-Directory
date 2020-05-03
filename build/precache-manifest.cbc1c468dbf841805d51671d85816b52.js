877e4fbafd8479bd01ed744c4589101b89825f56
Ruonan Y <joyceyang0730@gmail.com>
2020-05-03 14:21:09 -0700

    build and gitignore

---
+++ build/precache-manifest.cbc1c468dbf841805d51671d85816b52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1def08a29648a580a5b3174dc4c3cb97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d30f8f6e0d2f196f1f8",
+    "url": "/static/css/main.58e42695.chunk.css"
+  },
+  {
+    "revision": "517808393e55e141fa33",
+    "url": "/static/js/2.e7c78cef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7c78cef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d30f8f6e0d2f196f1f8",
+    "url": "/static/js/main.caf5812b.chunk.js"
+  },
+  {
+    "revision": "d157ba1ec312882390d3",
+    "url": "/static/js/runtime-main.14692623.js"
+  }
+]);
